9fc44179da91c12f43e3b67f2b0d2bb13fcebd7f
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2025-12-23 20:55:52 +0500

    Update article_to_speech.py

--- article_to_speech.py
+++ article_to_speech.py
@@ -1,16 +1,44 @@
 import os
 import pdfplumber
 from gtts import gTTS
+from flask import Flask, request, send_file
 
-article_File = pdf.plumber.open("article.pdf")
-article_content = []
+app = Flask(__name__)
+def extracted_text():
+    pdf_File = pdfplumber.open("sample.pdf")
+    article_content = []
 
-for page in article_File.pages:
-    article_text = page.extract_text()
-    if article_text:
-        article_content = article_content.append(article_text)
-article_File.close()
+    for page in pdf_File.pages:
+        article_text = page.extract_text()
+        if article_text:
+            article_content.append(article_text)
+    pdf_File.close()
+    return " ".join(article_content)
 
-full_article = " ".join(article_content)
-speech = gTTS(full_article, lang='en', slow = False)
-speech.save("article_audio.mp3:)")
+supported_languages = {
+    "English": "en",
+    "Spanish": "es",
+    "French": "fr",
+    "German": "de",
+    "Italian": "it",
+    "Chinese": "zh-CN",
+    "Japanese": "ja",
+    "Russian": "ru",
+    "Urdu": "ur" ,
+    "Hindi": "hi",
+    "Korean": "ko",
+    "Farsi": "fa"
+}
+
+@app.route("/tts", methods = ["POST"]) # This tells flask when /tts is requested and when to run function
+def text_to_speech():
+    data = request.json
+    language = data.get("language", "English")
+    text = extracted_text()
+
+    lang_code = Supported_Languages.get(language, "en")
+    tts = gTTS(text=text, lang=lang_code)
+    audio_file = "output.mp3"
+    tts.save(audio_file)
+
+    return send_file(audio_file, as_attachment=True)
