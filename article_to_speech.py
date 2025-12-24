994c739a73f1346136a82fdd0e3962413a0810ea
Ayaan Atif <140044111+ayaanatifcodes@users.noreply.github.com>
2025-12-24 19:06:05 +0500

    Update article_to_speech.py

--- article_to_speech.py
+++ article_to_speech.py
@@ -1,13 +1,12 @@
 import os
 import pdfplumber
-from gtts import gTTS
-from flask import Flask, request, send_file
-
+from flask import request, send_file, Flask, jsonify
 app = Flask(__name__)
+
 def extract_text():
     pdf_File = pdfplumber.open("sample.pdf")
     article_content = []
-    for page in pdf_FIle.pages:
+    for page in pdf_File.pages:
         article_text = page.extract_text()
         if article_text:
             article_content.append(article_text)
@@ -15,26 +14,22 @@ def extract_text():
     return " ".join(article_content)
 
 supported_languages = {
-    'en': 'English',
-    'es': 'Spanish',
-    'fr': 'French',
-    'de': 'German',
-    'it': 'Italian',
-    'pt': 'Portuguese',
-    'zh-cn': 'Chinese (Simplified)',
-    'ja': 'Japanese',
-    'ko': 'Korean'
+    "en": "English",
+    "es": "Spanish",
+    "fr": "French",
+    "de": "German",
+    "zh": "Chinese",
+    "jp": "Japanese"
 }
-
-@app.route('/tts', methods = ['POST'])
+@app.route("/tts", methods = ["POST"])
 def text_to_speech():
-    data = request.json()
-    language = data.get('language', 'English')
+    data = request.json
+    language = data.get("language", "en")
     text = extract_text()
-    lang_code = supported_languages.get(language, 'en')
-    tts = gTTS(text = text, lang = lang_code)
+    lang_code = supported_languages.get(language, "English")
+    tts = gTTS(text = text, lang)
     tts.save("output.mp3")
-    return send_file("output.mp3", as_attachment = True)
+    return send_file("output.mp3", as_attachment=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug = True)
